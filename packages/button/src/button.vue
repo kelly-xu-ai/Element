cee10c2462906a8d71a076b3b8a5425610a2f040
xulei <18039671980@163.com>
2020-03-18 10:44:24 +0800

    test

--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="el-button mytest01"
+    class="el-button mytest02"
     @click="handleClick"
     :disabled="buttonDisabled || loading"
     :autofocus="autofocus"

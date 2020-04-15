548f3deb2ec209a105ba62b49bf4cbcde3d6b1ba
xulei <18039671980@163.com>
2020-04-15 17:41:32 +0800

    优化

--- examples/route.config.js
+++ examples/route.config.js
@@ -1,6 +1,5 @@
 import navConfig from './nav.config';
 import navMyConfig from './nav.my.config';
-// import langs from './i18n/route';
 
 const LOAD_MAP = {
   'zh-CN': name => {
@@ -133,65 +132,7 @@ const registerMyRoute = (navMyConfig) => {
 };
 
 let route = registerRoute(navConfig);
-let myRoute = registerMyRoute(navMyConfig)
-
-// const generateMiscRoutes = function(lang) {
-//   let guideRoute = {
-//     path: `/${ lang }/guide`, // 指南
-//     redirect: `/${ lang }/guide/design`,
-//     component: load(lang, 'guide'),
-//     children: [{
-//       path: 'design', // 设计原则
-//       name: 'guide-design' + lang,
-//       meta: { lang },
-//       component: load(lang, 'design')
-//     }, {
-//       path: 'nav', // 导航
-//       name: 'guide-nav' + lang,
-//       meta: { lang },
-//       component: load(lang, 'nav')
-//     }]
-//   };
-
-//   let themeRoute = {
-//     path: `/${ lang }/theme`,
-//     component: load(lang, 'theme-nav'),
-//     children: [
-//       {
-//         path: '/', // 主题管理
-//         name: 'theme' + lang,
-//         meta: { lang },
-//         component: load(lang, 'theme')
-//       },
-//       {
-//         path: 'preview', // 主题预览编辑
-//         name: 'theme-preview-' + lang,
-//         meta: { lang },
-//         component: load(lang, 'theme-preview')
-//       }]
-//   };
-
-//   let resourceRoute = {
-//     path: `/${ lang }/resource`, // 资源
-//     meta: { lang },
-//     name: 'resource' + lang,
-//     component: load(lang, 'resource')
-//   };
-
-//   let indexRoute = {
-//     path: `/${ lang }`, // 首页
-//     meta: { lang },
-//     name: 'home' + lang,
-//     redirect: `/${ lang }/component/installation`
-//     // component: load(lang, 'index')
-//   };
-
-//   return [guideRoute, resourceRoute, themeRoute, indexRoute];
-// };
-
-// langs.forEach(lang => {
-//   route = route.concat(generateMiscRoutes(lang.lang));
-// });
+let myRoute = registerMyRoute(navMyConfig);
 
 route.push({
   path: '/play',
